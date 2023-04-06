db9b33cae3b88d5633341b7d3c0760b60aa441a6
AVI-Panwar <69872224+AVI-Panwar@users.noreply.github.com>
2023-04-06 20:24:20 +0530

    Initial commit

---
+++ Game.js
@@ -0,0 +1,41 @@
+let user=prompt("Enter S, W OR G");
+let cpuInput=Math.floor(Math.random() * 3);
+let cpu=["S", "W", "G"] [cpuInput];
+
+const match=(cpu, user)=>{
+      if(cpu===user)
+      {
+        return "Nobody";
+      }
+      else if(cpu==="S" && user==="W")
+      {
+        return "cpu";
+      }
+      else if(cpu==="S" && user==="G")
+      {
+        return "user";
+      }
+      else if(cpu==="G" && user==="W")
+      {
+        return "user";
+      }
+      else if(cpu==="G" && user==="S")
+      {
+        return "cpu";
+      }
+      else if(cpu==="W" && user==="G")
+      {
+        return "cpu";
+      }
+      else if(cpu==="W" && user==="S")
+      {
+        return "user";
+      }
+
+
+
+}
+
+
+let result = match(cpu, user);
+document.write(`CPU: ${cpu} <br> User: ${user} <br> The Winner is ${result}`);
